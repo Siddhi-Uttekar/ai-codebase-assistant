bd09caa545b2fed104ab32d8a953ac26c009828b
siddhi uttekar <158078551+Siddhi-Uttekar@users.noreply.github.com>
2026-03-14 18:21:25 +0530

    added read token

--- backend/embeddings.py
+++ backend/embeddings.py
@@ -1,10 +1,28 @@
+# from langchain_huggingface import HuggingFaceEmbeddings
+
+# def load_embeddings():
+
+#     embeddings = HuggingFaceEmbeddings(
+#         model_name="BAAI/bge-small-en-v1.5"
+            
+#     )
+
+#     return embeddings
+import os
+from huggingface_hub import login
 from langchain_huggingface import HuggingFaceEmbeddings
+
 
 def load_embeddings():
 
+    # Get token from Render environment variable
+    token = os.getenv("HF_TOKEN")
+
+    if token:
+        login(token)
+
     embeddings = HuggingFaceEmbeddings(
         model_name="BAAI/bge-small-en-v1.5"
-            
     )
 
     return embeddings
