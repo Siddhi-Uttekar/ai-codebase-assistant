9049c8b9c01c5726ecb334b990164446d2cb7898
siddhi uttekar <158078551+Siddhi-Uttekar@users.noreply.github.com>
2026-03-15 23:28:57 +0530

    Update vector_store.py

--- backend/vector_store.py
+++ backend/vector_store.py
@@ -1,13 +1,40 @@
+# from langchain_community.vectorstores import FAISS
+
+
+# def create_vector_store(chunks, embeddings):
+
+#     text_embeddings = list(zip(chunks, embeddings))
+
+#     vector_store = FAISS.from_embeddings(
+#         text_embeddings,
+#         embedding=None
+#     )
+
+#     vector_store.save_local("vector_db")
+
+#     return vector_store
+
+
+# def load_vector_store():
+
+#     vector_store = FAISS.load_local(
+#         "vector_db",
+#         embeddings=None,
+#         allow_dangerous_deserialization=True
+#     )
+
+#     return vector_store
 from langchain_community.vectorstores import FAISS
+from embeddings import VoyageEmbeddings
 
 
-def create_vector_store(chunks, embeddings):
+def create_vector_store(chunks):
 
-    text_embeddings = list(zip(chunks, embeddings))
+    embeddings = VoyageEmbeddings()
 
-    vector_store = FAISS.from_embeddings(
-        text_embeddings,
-        embedding=None
+    vector_store = FAISS.from_texts(
+        chunks,
+        embedding=embeddings
     )
 
     vector_store.save_local("vector_db")
@@ -17,9 +44,11 @@ def create_vector_store(chunks, embeddings):
 
 def load_vector_store():
 
+    embeddings = VoyageEmbeddings()
+
     vector_store = FAISS.load_local(
         "vector_db",
-        embeddings=None,
+        embeddings=embeddings,
         allow_dangerous_deserialization=True
     )
 
