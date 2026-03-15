133dee1f648e61c85f89e51af21fa1abefbf3849
siddhi uttekar <158078551+Siddhi-Uttekar@users.noreply.github.com>
2026-03-15 18:49:09 +0530

    Update embeddings.py

--- backend/embeddings.py
+++ backend/embeddings.py
@@ -1,9 +1,11 @@
+
+
 import requests
 import os
 
 VOYAGE_API_KEY = os.getenv("VOYAGE_API_KEY")
 
-def load_embeddings(text):
+def load_embeddings(texts):
 
     response = requests.post(
         "https://api.voyageai.com/v1/embeddings",
@@ -13,7 +15,7 @@ def load_embeddings(text):
         },
         json={
             "model": "voyage-2",
-            "input": [text]
+            "input": texts
         }
     )
 
@@ -22,4 +24,4 @@ def load_embeddings(text):
     if "data" not in data:
         raise Exception(f"Voyage API Error: {data}")
 
-    return data["data"][0]["embedding"]
+    return [item["embedding"] for item in data["data"]]
