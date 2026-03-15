c3e5d73e09481201e83c57082c5070940c6290dc
siddhi uttekar <158078551+Siddhi-Uttekar@users.noreply.github.com>
2026-03-15 23:18:49 +0530

    Update vector_store.py

--- backend/vector_store.py
+++ backend/vector_store.py
@@ -1,25 +1,25 @@
 from langchain_community.vectorstores import FAISS
 
-from langchain_community.vectorstores import FAISS
 
 def create_vector_store(chunks, embeddings):
 
     text_embeddings = list(zip(chunks, embeddings))
 
     vector_store = FAISS.from_embeddings(
-        text_embeddings, embedding=None
+        text_embeddings,
+        embedding=None
     )
 
     vector_store.save_local("vector_db")
 
     return vector_store
 
-from langchain_community.vectorstores import FAISS
 
 def load_vector_store():
 
     vector_store = FAISS.load_local(
         "vector_db",
+        embeddings=None,
         allow_dangerous_deserialization=True
     )
 
