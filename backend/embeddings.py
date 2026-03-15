083d03a3e36d79baae1563b516a2db0f27b211ce
siddhi uttekar <158078551+Siddhi-Uttekar@users.noreply.github.com>
2026-03-15 23:28:32 +0530

    Update embeddings.py

--- backend/embeddings.py
+++ backend/embeddings.py
@@ -1,27 +1,65 @@
 
 
-import requests
+# import requests
+# import os
+
+# VOYAGE_API_KEY = os.getenv("VOYAGE_API_KEY")
+
+# def load_embeddings(texts):
+
+#     response = requests.post(
+#         "https://api.voyageai.com/v1/embeddings",
+#         headers={
+#             "Authorization": f"Bearer {VOYAGE_API_KEY}",
+#             "Content-Type": "application/json"
+#         },
+#         json={
+#             "model": "voyage-2",
+#             "input": texts
+#         }
+#     )
+
+#     data = response.json()
+
+#     if "data" not in data:
+#         raise Exception(f"Voyage API Error: {data}")
+
+#     return [item["embedding"] for item in data["data"]]
 import os
+import requests
+from langchain_core.embeddings import Embeddings
 
-VOYAGE_API_KEY = os.getenv("VOYAGE_API_KEY")
+class VoyageEmbeddings(Embeddings):
 
-def load_embeddings(texts):
+    def embed_documents(self, texts):
+        vectors = []
+        for text in texts:
+            res = requests.post(
+                "https://api.voyageai.com/v1/embeddings",
+                headers={
+                    "Authorization": f"Bearer {os.getenv('VOYAGE_API_KEY')}",
+                    "Content-Type": "application/json"
+                },
+                json={
+                    "input": text,
+                    "model": "voyage-code-2"
+                }
+            )
+            vectors.append(res.json()["data"][0]["embedding"])
 
-    response = requests.post(
-        "https://api.voyageai.com/v1/embeddings",
-        headers={
-            "Authorization": f"Bearer {VOYAGE_API_KEY}",
-            "Content-Type": "application/json"
-        },
-        json={
-            "model": "voyage-2",
-            "input": texts
-        }
-    )
+        return vectors
 
-    data = response.json()
+    def embed_query(self, text):
+        res = requests.post(
+            "https://api.voyageai.com/v1/embeddings",
+            headers={
+                "Authorization": f"Bearer {os.getenv('VOYAGE_API_KEY')}",
+                "Content-Type": "application/json"
+            },
+            json={
+                "input": text,
+                "model": "voyage-code-2"
+            }
+        )
 
-    if "data" not in data:
-        raise Exception(f"Voyage API Error: {data}")
-
-    return [item["embedding"] for item in data["data"]]
+        return res.json()["data"][0]["embedding"]
